5cdf442ed16b38d35cfbe8fd917f13374929d4bc
ARA <mon0mon@naver.com>
2023-11-29 21:08:38 +0900

    ✨ feat : Ch04 데이터베이스 액세스

---
+++ modules/ch04/src/test/java/xyz/mon0mon/ch04/Ch04ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.ch04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
