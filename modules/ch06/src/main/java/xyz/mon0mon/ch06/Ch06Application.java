58e7a6d5f9e3193f5f0ad7ba5eec543818a9388d
ARA <mon0mon@naver.com>
2023-12-05 23:20:09 +0900

    ✨ feat : Ch06 데이터 파고들기 - Spring Data Template

---
+++ modules/ch06/src/main/java/xyz/mon0mon/ch06/Ch06Application.java
@@ -0,0 +1,31 @@
+package xyz.mon0mon.ch06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@SpringBootApplication
+public class Ch06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch06Application.class, args);
+    }
+
+    @Bean
+    public RedisOperations<String, Aircraft> redisOperations(RedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<Aircraft> serializer = new Jackson2JsonRedisSerializer<>(
+                Aircraft.class);
+
+        RedisTemplate<String, Aircraft> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setDefaultSerializer(serializer);
+        template.setKeySerializer(new StringRedisSerializer());
+
+        return template;
+    }
+}
